fd789e90d1d9abbf9d916d3f432b4f72f6250569
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2021-09-26 23:33:42 -0300

    food refactored

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -2,12 +2,12 @@ import { Component } from 'react';
 import React, { useState, useEffect } from 'react';
 import {Header} from '../../components/Header';
 import { api } from '../../services/api';
-import Food from '../../components/Food';
+import {Food} from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
 import { FoodsContainer } from './styles';
 
-interface FoodProps {
+export interface FoodProps {
   id: number;
   name: string;
   description: string;
