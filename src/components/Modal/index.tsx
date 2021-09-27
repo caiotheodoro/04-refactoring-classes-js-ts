c449a813d9076003337cd0ca25cf2d0a0caec94a
caiotheodoro <caio-eduardotheodoro@hotmail.com>
2021-09-26 23:23:07 -0300

    modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,28 +1,19 @@
-import { Component } from 'react';
+import {ReactNode } from 'react';
 import ReactModal from 'react-modal';
+import React, {useState, useEffect} from 'react'
 
-class Modal extends Component {
-  constructor(props) {
-    super(props);
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: () => void;
+  children: ReactNode;
+}
 
-    const { isOpen } = this.props;
-    this.state = {
-      modalStatus: isOpen
-    }
-  }
+export function Modal ({isOpen, setIsOpen, children}: ModalProps){
+  const [modalStatus, setModalStatus] = useState(isOpen);
 
-  componentDidUpdate(prevProps) {
-    const { isOpen } = this.props;
-
-    if (prevProps.isOpen !== isOpen) {
-      console.log(this.props)
-      this.setState({ modalStatus: isOpen })
-    }
-  }
-
-  render() {
-    const { children, setIsOpen } = this.props;
-    const { modalStatus } = this.state;
+  useEffect(() => {
+    setModalStatus(isOpen);
+  }, [isOpen]);
 
     return (
       <ReactModal
@@ -52,7 +43,5 @@ class Modal extends Component {
         {children}
       </ReactModal>
     );
-  }
+  
 };
-
-export default Modal;
